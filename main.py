e453179fc68eca266dc98d99d2923727a12c0845
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2021-11-22 09:43:38 -0300

    Implementar opções 2 a 4 no menu

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ if __name__ == '__main__':
     tam = 1
     while tam % 2:
         try:
-            tam = int(input("Tamanho do espaço 2D quadrado (inteiro ímpar): "))
+            tam = int(input('Tamanho do espaço 2D quadrado (inteiro ímpar): '))
         except ValueError:
-            print("\nValor incorreto! Digite um número inteiro ímpar.\n")
+            print('\nValor incorreto! Digite um número inteiro ímpar.\n')
             continue
 
     espaco = Space2D(size=tam)
 
-    entrada = ""
-    while entrada != "5":
+    entrada = ''
+    while entrada != '0':
         entrada = input(f"""
     Tamanho do espaço: {tam}
     Obs: um espaço de tamanho {tam} tem coordenadas de -{tam//2} a {tam//2}.
-    
+
     Menu:
         1 - Desenhar um objeto.
         2 - Inserir uma carga num objeto.
@@ -26,19 +26,24 @@ if __name__ == '__main__':
         4 - Consultar as informações de um ponto.
         5 - Gerar as visualizações.
         0 - Sair.
-        
+
         Opção: """)
-        if entrada == 0:
+        if entrada == '0':  # OPÇÃO SAIR
+            print('Encerrando simulador.')
             break
-        elif entrada == 1:
+        elif entrada == '1':  # OPÇÃO DESENHAR OBJETO
+            # repete até o usuário escolher 1 ou 2.
             obj = input(
-                "Que objeto deseja inserir? 1 - círculo, 2 - semiplano: ").strip()
-            while obj not in ("1", "2"):
-                print("Valor inválido.")
+                'Que objeto deseja inserir? 1 - círculo, 2 - semiplano, 0 - cancelar: ').strip()
+            while obj not in ('1', '2', '0'):
+                print('Valor inválido.')
                 obj = input(
-                    "Que objeto deseja inserir? 1 - círculo, 2 - semiplano: ").strip()
-            if obj == "1":
-                mat = ""
+                    'Que objeto deseja inserir? 1 - círculo, 2 - semiplano, 0 - cancelar: ').strip()
+
+            if obj == '0':  # repete o menu, cancelando operacao
+                continue
+            elif obj == '1':  # desenhar circulo
+                mat = ''
                 while mat not in ('V', 'C', 'I'):
                     mat = input(
                         'Tipo do material ("V" para vácuo, "C" para condutor e "I" para isolante): ').strip().upper()
@@ -65,8 +70,8 @@ if __name__ == '__main__':
                 espaco.desenha_circulo(material=Ponto(mat), coord_centro=(
                     posx, posy), raio=raio_circulo)
                 # chamar inserir_carga_em_objeto() em um ponto do círculo
-            elif obj == "2":
-                mat = ""
+            elif obj == '2':  # desenhar semiplano
+                mat = ''
                 while mat not in ('V', 'C', 'I'):
                     mat = input(
                         'Tipo do material ("V" para vácuo, "C" para condutor e "I" para isolante): ').strip().upper()
@@ -113,4 +118,48 @@ if __name__ == '__main__':
                 # perguntar os limites do semiplano
                 # chamar desenhar_semiplano()
                 # chamar inserir_carga_em_objeto() em um ponto do círculo
-                pass
+        elif entrada == '2':  # OPÇÃO INSERIR CARGA EM OBJETO
+            try:
+                carga = float(
+                    input('Digite a carga em Coulombs a ser inserida no objeto: '))
+                posx = int(
+                    input('Digite a coordenada x de um ponto pertencente ao objeto: ')) + (espaco.size//2)
+                posy = int(input(
+                    'Digite a coordenada y do ponto pertencente ao objeto: ')) + (espaco.size//2)
+                if not espaco.validIndex(coord=(posx, posy)):
+                    raise ValueError
+            except ValueError:
+                print("Valores inválidos. Voltando ao menu.")
+                continue
+            espaco.inserir_carga_em_objeto(
+                ponto_inicial=(posx, posy), carga=carga)
+        elif entrada == '3':  # OPÇÃO INSERIR CARGA PONTUAL
+            try:
+                carga = float(
+                    input('Digite a carga em Coulombs a ser inserida no ponto do espaço: '))
+                posx = int(
+                    input('Digite a coordenada x de um ponto: ')) + (espaco.size//2)
+                posy = int(input(
+                    'Digite a coordenada y do ponto: ')) + (espaco.size//2)
+                if not espaco.validIndex(coord=(posx, posy)):
+                    raise ValueError
+            except ValueError:
+                print("Valores inválidos. Voltando ao menu.")
+                continue
+            espaco.inserir_carga_pontual(coord=(posx, posy), carga=carga)
+        elif entrada == '4':  # OPÇÃO CONSULTAR INFORMAÇÕES DE UM PONTO
+            try:
+                posx = int(
+                    input('Digite a coordenada x de um ponto: ')) + (espaco.size//2)
+                posy = int(input(
+                    'Digite a coordenada y do ponto: ')) + (espaco.size//2)
+                if not espaco.validIndex(coord=(posx, posy)):
+                    raise ValueError
+            except ValueError:
+                print("Valores inválidos. Voltando ao menu.")
+                continue
+            print('Informações do ponto escolhido:')
+            print(espaco.points[posx][posy])
+        elif entrada == '5':  # OPÇÃO GERAR VISUALIZAÇÕES
+            # TODO: inserir essas funções de gerar visualização aqui
+            pass
